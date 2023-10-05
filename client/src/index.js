6bbf0b5bcd61c237aa09b6ff2693280c21ba1b47
Priyanshu <priyanshunita15072002@gmail.com>
2023-10-05 12:23:26 +0530

    #link corrected

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 
 
-export const server = 'https://blogging-website-backend.vercel.app/'
+export const server = 'https://blogging-website-backend.vercel.app'
 // export const server = 'http://localhost:5000' ;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
